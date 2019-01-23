be34d3030ad84d0715a86b03d9cffdc72fa391a4
Nikolay Samoylov <nsammi@googlemail.com>
2019-01-23 14:59:44 +0200

    Initial Commit

---
+++ Jellyfish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jellyfish
+//
+//  Created by Nick Samoylov on 1/23/19.
+//  Copyright © 2019 Nick Samoylov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
